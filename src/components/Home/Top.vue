69013b7f2ed6dbda94375f75b1c7c95b0a0b33dc
Luan Eli Oliveira <luannn@gmail.com>
2019-04-04 22:27:12 -0300

    Fix | Ajuste do layout da página inicial

--- src/components/Home/Top.vue
+++ src/components/Home/Top.vue
@@ -8,8 +8,8 @@
       column
       justify-center
     >
-      <h1 class="display-3 font-weight-thin mb-3">Agile Joinville</h1>
-      <h4 class="subheading mb-4">Participe dos nosso encontros e fique por dentro das novidades!</h4>
+      <h1 class="display-3 font-weight-thin">Agile Joinville</h1>
+      <h4 class="mb-3">Participe dos nosso encontros e fique por dentro das novidades!</h4>
       <v-btn large outline color="white" href="https://www.meetup.com/pt-BR/Agile-Joinville/">Meetup Agile Joinville</v-btn>
     </v-layout>
   </v-parallax>
