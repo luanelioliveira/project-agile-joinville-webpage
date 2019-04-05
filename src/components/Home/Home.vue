f43fd6faa75517827c9da1d62d4fff6c04c17872
Luan Eli Oliveira <luannn@gmail.com>
2019-04-04 22:24:24 -0300

    Fix | Retirado código inutilizado

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -19,46 +19,6 @@ export default {
     Sponsors,
     Jobs,
     Footer
-  },
-  data () {
-    return {
-      socials: [
-        'fab fa-facebook',
-        'fab fa-instagram',
-        'fab fa-linkedin'
-      ],
-      vagas: [
-        {
-          id: 1,
-          papel : 'Product Owner',
-          empresa : 'FESC Tecnologia',
-          contato : 'luannn@gmail.com',
-          dataPublicada: '04/04/2019',
-          descricao: 'Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.'
-        },
-        {
-          id: 2,
-          papel : 'Engenheiro de Software',
-          empresa : 'Magrathea Labs',
-          contato : 'luannn@gmail.com',
-          dataPublicada: '04/04/2019',
-          descricao: 'Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.'
-        },
-        {
-          id: 3,
-          papel : 'Scrum Master',
-          empresa : 'Mercos',
-          contato : 'luannn@gmail.com',
-          dataPublicada: '04/04/2019',
-          descricao: 'Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.'
-        }
-      ]
-    }
-  },  
-  computed: {
-    loading () {
-      return this.$store.getters.loading
-    }
   }
 }
 </script>
