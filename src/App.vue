fdc223ff62383b6a17feaab55925949548ad6794
Luan Eli Oliveira <luannn@gmail.com>
2019-04-07 21:20:56 -0300

    Fix | Criado componente para a toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app flat dark>   
-      <v-toolbar-title>   
-        <router-link to="/" tag="span" style="cursor: pointer">
-          <span class="font-weight-medium ">AGILE&nbsp;</span>
-          <span class="font-weight-thin">JOINVILLE</span>
-        </router-link>        
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-        <v-btn small flat v-if="!isAuthenticated" to="/about"><span>Sobre</span></v-btn>
-        <v-btn small flat v-if="!isAuthenticated" to="/jobs"><span>Vagas</span></v-btn>
-        <v-btn small outline v-if="!isAuthenticated" to="/signin"><span>Login</span></v-btn>
-        <v-btn small flat v-if="isAuthenticated" to="/profile"><span>Perfil</span></v-btn>
-        <v-btn small flat v-if="isAuthenticated" @click="onLogout"><span>Logout</span></v-btn>
-    </v-toolbar>
+    <MenuApp />
     <v-content>
       <router-view/>
     </v-content>
@@ -21,19 +8,12 @@
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import MenuApp from '@/components/App/MenuApp'
 
 export default {
   name: 'App',
-  computed: {
-    ...mapState("Authentication", ["isAuthenticated"])
-  },
-  methods: {
-    ...mapActions('Authentication', ['signOut']),
-    onLogout () {
-      this.signOut()
-      this.$router.push('/')
-    }
+  components: {
+    MenuApp
   }
 }
 </script>
