ee1d3e7985a3687028c8fd5ae3b5de79e073c22a
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-07-17 18:35:16 -0300

    Ajuste do css dos cards dos itens

--- src/component/ItensPatrocinados.vue
+++ src/component/ItensPatrocinados.vue
@@ -143,3 +143,13 @@ export default {
 }
 </script>
 
+<style> 
+  .v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .5;
+    position: absolute;
+    width: 100%;
+  }
+</style>
