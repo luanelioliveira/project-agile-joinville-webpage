64c0474e93571fd5017804450ba21b3be0fb07e2
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-07-17 18:35:00 -0300

    Refatorada sessao da divulgacao do evento

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,8 @@
       <Localizacao />      
       <Patrocinadores />  
       <ItensPatrocinados />   
-      <DivulgacaoApoio />     
-      
-      <section>
-        <v-parallax src="https://images.pexels.com/photos/2376995/pexels-photo-2376995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" height="380">
-          <v-layout column align-center justify-center>
-            <div class="display-2 font-weight-thin white--text mb-3 text-xs-center">Venha participar e adiquirir novos conhecimentos!</div>
-            <span class="font-weight-bold font-italic">"Compartilhar conhecimento é o caminho mais próximo da sabedoria..."</span>
-            <v-btn class="black mt-5" dark flat round depressed large v-on:click="openPage(inscricoes)">
-              FAZER MINHA INSCRIÇÃO
-            </v-btn>            
-          </v-layout>
-        </v-parallax>
-      </section>
+      <DivulgacaoApoio />   
+      <DivulgacaoEvento />           
       <Organizacao /> 
       <Rodape /> 
     </v-content>
@@ -34,6 +23,7 @@ import Localizacao from './component/Localizacao'
 import Patrocinadores from './component/Patrocinadores'
 import ItensPatrocinados from './component/ItensPatrocinados'
 import DivulgacaoApoio from './component/DivulgacaoApoio'
+import DivulgacaoEvento from './component/DivulgacaoEvento'
 import Organizacao from './component/Organizacao'
 import Rodape from './component/Rodape'
 
@@ -46,34 +36,10 @@ export default {
     Patrocinadores,
     ItensPatrocinados,
     DivulgacaoApoio,
+    DivulgacaoEvento,
     Organizacao,
     Rodape
   },
-  name: 'App',
-  data () {
-    return {
-        email: 'agilejoinville@gmail.com',
-        inscricoes: 'https://inscricoes.agilebrazil.com/',
-    }
-  },
-  methods : {
-    openPage(page) {
-      window.open(page,'_blank');
-    },
-    sendMail(email) {
-      this.openPage('mailto:' + email);
-    }
-  }
+  name: 'App',  
 }
 </script>
-
-<style>
-.v-card--reveal {
-  align-items: center;
-  bottom: 0;
-  justify-content: center;
-  opacity: .5;
-  position: absolute;
-  width: 100%;
-}
-</style>
---
+++ src/component/DivulgacaoEvento.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <v-parallax src="https://images.pexels.com/photos/2376995/pexels-photo-2376995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" height="380">
+      <v-layout column align-center justify-center>
+        <div class="display-2 font-weight-thin white--text mb-3 text-xs-center">Venha participar e adiquirir novos conhecimentos!</div>
+        <span class="font-weight-bold font-italic">"Compartilhar conhecimento é o caminho mais próximo da sabedoria..."</span>
+        <v-btn class="black mt-5" dark flat round depressed large v-on:click="openPage(inscricoes)">
+          FAZER MINHA INSCRIÇÃO
+        </v-btn>            
+      </v-layout>
+    </v-parallax>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      inscricoes: 'https://inscricoes.agilebrazil.com/',
+    }
+  },
+  methods : {
+    openPage(page) {
+      window.open(page,'_blank');
+    },
+  },
+}
+</script>
+
+
